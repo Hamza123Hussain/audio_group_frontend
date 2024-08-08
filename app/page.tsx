abd29a6b9722c3b4cac07c6a6733689dfdc36858
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-08 19:06:41 +0500

    made a list of tasks that are to be completed and then move forward in the video

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,11 @@ import { useAuth0 } from '@auth0/auth0-react'
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0()
-
+  // side nav will added here as no layout needed as sidenav needed on home pg only
+  // we use usestate and index to modidy what appears on the right side of our sidenav
+  // this page will only be shown if user is authenticated
+  // landing page to made first
+  // do all the above and then move to the next part of the video
   return <button onClick={() => loginWithRedirect()}>Log In</button>
 }
 
