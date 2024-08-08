3a5d2b3cddfbeb81f3670a53bfefa33217bc48bf
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-08 16:29:43 +0500

    calling the user profile componenet

---
+++ app/Users/page.tsx
@@ -0,0 +1,12 @@
+import UserProfile from '@/components/UserPage'
+import React from 'react'
+
+const USERS = () => {
+  return (
+    <div>
+      <UserProfile />
+    </div>
+  )
+}
+
+export default USERS
