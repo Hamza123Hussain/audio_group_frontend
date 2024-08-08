430f289a105db3522db6df2b9753270edea7e1ed
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-08 19:07:08 +0500

    this component will be removed soon as it is not needed

--- app/Users/page.tsx
+++ app/Users/page.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 const USERS = () => {
   return (
     <div>
+      {/* this componenet will be shifted to the home page as it contains the info needed to show whne the user is auth */}
       <UserProfile />
     </div>
   )
