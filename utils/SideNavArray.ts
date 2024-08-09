fabbe824b2427e9cfeff6e5ffe3204f77dcc645a
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-09 19:45:35 +0500

    adding index

--- utils/SideNavArray.ts
+++ utils/SideNavArray.ts
@@ -5,26 +5,31 @@ const sideNavItems = [
     name: 'Home',
     path: '/home',
     icon: FiHome,
+    index: 1,
   },
   {
     name: 'Previous',
     path: '/previous-meetings',
     icon: FiCalendar,
+    index: 2,
   },
   {
     name: 'Upcoming',
     path: '/upcoming-meetings',
     icon: FiClock,
+    index: 3,
   },
   {
     name: 'Recordings',
     path: '/recordings',
     icon: FiVideo,
+    index: 4,
   },
   {
     name: 'Personal Room',
     path: '/personal-room',
     icon: FiUser,
+    index: 5,
   },
 ]
 
