8e07c74a5fd06c61aa0bdb3912d1fe35e3a09d76
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-10 03:46:04 +0500

    adding upcoming meetings

--- components/SideComponents/Home.tsx
+++ components/SideComponents/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import CurrentDateTime from '../CurrenTimeandDate'
 import TheMeetingsCards from './Home/TheMeetingsCards'
+import { FaCalendarAlt } from 'react-icons/fa'
 
 const Home = () => {
   return (
@@ -15,8 +16,21 @@ const Home = () => {
 
         <CurrentDateTime />
       </div>
-      <div className=" grid grid-cols-1 sm:grid-cols-4 mt-5  px-4 gap-10 justify-between">
+      <div className=" grid grid-cols-2 sm:grid-cols-2 justify-center md:grid-cols-4 mt-5 md:px-4   gap-10 items-center md:justify-between">
         <TheMeetingsCards />
+      </div>
+      <div className=" flex flex-col px-4 mt-5 gap-2">
+        <h1 className=" font-extrabold text-2xl">Upcoming Meetings</h1>
+        <div className=" w-full flex flex-col bg-slate-800 rounded-lg p-3">
+          <h1>
+            <FaCalendarAlt />
+          </h1>
+          <h1> Meeting Title</h1>
+          <div className=" flex gap-2">
+            <h1> Meeting Date</h1>
+            <h1>Meeting time</h1>
+          </div>
+        </div>
       </div>
     </div>
   )
