b2a0a29251e406c9708da07b985eacf5011ce796
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-09 16:32:36 +0500

    removed the unused code

--- components/UserPage.tsx
+++ components/UserPage.tsx
@@ -1,29 +1,25 @@
-'use client'
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 
 const UserProfile = () => {
-  const { user, isAuthenticated, isLoading, logout } = useAuth0()
+  const { user, isLoading, logout } = useAuth0()
 
   if (isLoading) {
     return <div>Loading ...</div>
   }
 
   return (
-    isAuthenticated && (
-      <div>
-        <img src={user?.picture} alt={user?.name} />
-        <h2>{user?.name}</h2>
-        <p>{user?.email}</p>
-        <button
-          onClick={() =>
-            logout({ logoutParams: { returnTo: window.location.origin } })
-          }
-        >
-          Log Out
-        </button>
-      </div>
-    )
+    <div className=" flex items-center gap-2">
+      <h2 className=" text-sm">{user?.name}</h2>
+      <button
+        className=" text-sm bg-black border-white px-2 rounded-lg text-white hover:shadow-sm hover:shadow-slate-900"
+        onClick={() =>
+          logout({ logoutParams: { returnTo: window.location.origin } })
+        }
+      >
+        Log Out
+      </button>
+    </div>
   )
 }
 
