958c104b9740bcdd21d1e7f7225643a9c3ca8ca8
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-09 19:41:35 +0500

    removed useless code and added clerk button

--- components/UserPage.tsx
+++ components/UserPage.tsx
@@ -1,23 +1,13 @@
+import { UserButton, useUser } from '@clerk/nextjs'
 import React from 'react'
-import { useAuth0 } from '@auth0/auth0-react'
 
 const UserProfile = () => {
-  const { user, isLoading, logout } = useAuth0()
-
-  if (isLoading) {
-    return <div>Loading ...</div>
-  }
-
+  const { user } = useUser()
   return (
     <div className=" flex items-center gap-2">
-      <h2 className=" text-sm">{user?.name}</h2>
-      <button
-        className=" text-sm bg-black border-white px-2 rounded-lg text-white hover:shadow-sm hover:shadow-slate-900"
-        onClick={() =>
-          logout({ logoutParams: { returnTo: window.location.origin } })
-        }
-      >
-        Log Out
+      <h1>{user?.fullName}</h1>
+      <button className=" text-sm  border-white px-2 rounded-lg text-white hover:shadow-sm hover:shadow-slate-900">
+        <UserButton />
       </button>
     </div>
   )
