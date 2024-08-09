97f556a3ccda37102ead3040c2db8b751f0a725b
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-10 03:25:09 +0500

    Getting date and time to be shown on the home screen

---
+++ components/CurrenTimeandDate.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const CurrentDateTime = () => {
+  const now = new Date()
+
+  // Format the date
+  const date = now.toLocaleDateString('en-US', {
+    weekday: 'long', // 'Monday'
+    year: 'numeric', // '2024'
+    month: 'long', // 'August'
+    day: 'numeric', // '10'
+  })
+
+  // Format the time
+  const time = now.toLocaleTimeString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+  })
+
+  return (
+    <div className="flex flex-col ">
+      <h1 className="  text-white text-6xl p-2"> {time}</h1>
+      <h1 className=" text-white px-4"> {date}</h1>
+    </div>
+  )
+}
+
+export default CurrentDateTime
