a83bfeada40d76bd2b7e145610658001cc374f37
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-09 19:41:17 +0500

    page temporary

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
-'use client'
 import React from 'react'
-import { useAuth0 } from '@auth0/auth0-react'
-import LandingPage from '@/components/LandingPage'
 import SideNav from '@/components/SideNav'
 
 const LoginButton = () => {
