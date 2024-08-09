809a23a7f8515a52ddf4dea3f96ff9a5c346c59d
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-09 20:01:11 +0500

    removing path as it is not needed

--- utils/SideNavArray.ts
+++ utils/SideNavArray.ts
@@ -1,33 +1,29 @@
 import { FiHome, FiCalendar, FiClock, FiVideo, FiUser } from 'react-icons/fi'
+import { SIDENAV } from './SideNavInterface'
 
-const sideNavItems = [
+const sideNavItems: SIDENAV[] = [
   {
     name: 'Home',
-    path: '/home',
     icon: FiHome,
     index: 1,
   },
   {
     name: 'Previous',
-    path: '/previous-meetings',
     icon: FiCalendar,
     index: 2,
   },
   {
     name: 'Upcoming',
-    path: '/upcoming-meetings',
     icon: FiClock,
     index: 3,
   },
   {
     name: 'Recordings',
-    path: '/recordings',
     icon: FiVideo,
     index: 4,
   },
   {
     name: 'Personal Room',
-    path: '/personal-room',
     icon: FiUser,
     index: 5,
   },
