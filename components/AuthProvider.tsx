500e8903bd5a9e5d08b1f4a61b671fda808fb43d
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-09 19:42:24 +0500

    removed lots of unused code

--- components/AuthProvider.tsx
+++ components/AuthProvider.tsx
@@ -1,44 +1,17 @@
 // AuthProvider.js
 'use client'
-
-import { Auth0Provider, useAuth0 } from '@auth0/auth0-react'
-import { useEffect, useState } from 'react'
+import { useUser } from '@clerk/nextjs'
 import LandingPage from './LandingPage'
-import Navbar from './Navbar'
-import Footer from './Footer'
 
 export default function AuthProvider({
   children,
 }: {
   children: React.ReactNode
 }) {
-  const [redirectUri, setRedirectUri] = useState('')
-  const { isAuthenticated } = useAuth0()
-  useEffect(() => {
-    // Set redirect URI to the user component
-    setRedirectUri(`${window.location.origin}/`)
-  }, [])
-
-  if (!redirectUri) {
-    return null // or a loading spinner
-  }
-
+  const { user } = useUser()
   return (
-    <Auth0Provider
-      domain="dev-4q611plcdtbmsn6z.us.auth0.com"
-      clientId="Ttiz8xnAzYH36yufQV2d77WRQY2aFf2h"
-      authorizationParams={{
-        redirect_uri: redirectUri,
-      }}
-    >
-      {' '}
-      <div className=" flex flex-col">
-        {/* navbar will come here */}
-        {/* <Navbar /> */}
-        <div>{!isAuthenticated ? <LandingPage /> : children} </div>{' '}
-        {/* footer will come here */}
-        {/* <Footer /> */}
-      </div>
-    </Auth0Provider>
+    <div className=" flex flex-col">
+      <div className=" flex-grow">{!user ? <LandingPage /> : children} </div>{' '}
+    </div>
   )
 }
