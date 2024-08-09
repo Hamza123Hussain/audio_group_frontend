fa42e4d5a8df3e7d289bd9df1df9eb2d70ebc72a
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-09 17:03:08 +0500

    temporaliy mounted landing page here

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
+import LandingPage from '@/components/LandingPage'
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0()
@@ -9,7 +10,7 @@ const LoginButton = () => {
   // this page will only be shown if user is authenticated
   // landing page to made first
   // do all the above and then move to the next part of the video
-  return <button onClick={() => loginWithRedirect()}>Log In</button>
+  return <LandingPage />
 }
 
 export default LoginButton
