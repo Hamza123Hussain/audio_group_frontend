7f138e5b069ebcd8d3d0b913e587ea56c214d373
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-09 16:14:23 +0500

    Navbar Mounted

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import AuthProvider from '@/components/AuthProvider'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,7 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <AuthProvider>
           {/* navbar will come here */}
-
+          <Navbar />
           {children}
           {/* footer will come here */}
         </AuthProvider>
