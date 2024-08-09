27d7ca574e0cea050d24767e374b70773088c1b0
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-09 18:09:16 +0500

    mounting sidenav in the home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,14 @@
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 import LandingPage from '@/components/LandingPage'
+import SideNav from '@/components/SideNav'
 
 const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0()
-  // side nav will added here as no layout needed as sidenav needed on home pg only
-  // we use usestate and index to modidy what appears on the right side of our sidenav
-  // this page will only be shown if user is authenticated
-  // landing page to made first
-  // do all the above and then move to the next part of the video
-  return <>hiii</>
+  return (
+    <div>
+      <SideNav />
+    </div>
+  )
 }
 
 export default LoginButton
