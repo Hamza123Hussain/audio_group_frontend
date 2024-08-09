b257b49db3e50cdb0258480b4d6b3b69b66f8c28
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-09 18:14:30 +0500

    removing unused componenets

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import AuthProvider from '@/components/AuthProvider'
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
-
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
