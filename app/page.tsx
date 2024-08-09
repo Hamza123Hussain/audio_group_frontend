24d382cbd0b52fd5545e10f5e794235de562ea06
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-09 20:00:08 +0500

    sidenav and side components merged into a flex on the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import SideNav from '@/components/SideNav'
+import Home from '@/components/SideComponents/Home'
+import Previous from '@/components/SideComponents/Previous'
+import Upcoming from '@/components/SideComponents/Upcoming'
+import { Recording } from '@/components/SideComponents/Recording'
+import Personal from '@/components/SideComponents/Personal'
 
 const LoginButton = () => {
+  const [index, setindex] = useState(0)
   return (
-    <div>
-      <SideNav />
+    <div className=" flex">
+      <SideNav index={index} setindex={setindex} />
+      <div className=" bg-slate-900 flex-1 text-white ">
+        {index == 0 ? (
+          <Home />
+        ) : index == 1 ? (
+          <Previous />
+        ) : index == 2 ? (
+          <Upcoming />
+        ) : index == 3 ? (
+          <Recording />
+        ) : index == 4 ? (
+          <Personal />
+        ) : (
+          ''
+        )}
+      </div>
     </div>
   )
 }
