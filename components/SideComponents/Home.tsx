e0789eb66f82ad64849e6a21b5c013fbe32ae52a
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-10 03:24:01 +0500

    home page component being made

--- components/SideComponents/Home.tsx
+++ components/SideComponents/Home.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import CurrentDateTime from '../CurrenTimeandDate'
+import TheMeetingsCards from './Home/TheMeetingsCards'
 
 const Home = () => {
-  return <div>gome</div>
+  return (
+    <div className=" p-4 flex-col flex ">
+      <div className=" p-4 bg-slate-950 rounded-lg text-white flex flex-col justify-between gap-10 items-start ">
+        <div>
+          {' '}
+          <h1 className=" bg-slate-500 rounded-lg text-black p-2">
+            Upcoming Meeting : 12:30 PM
+          </h1>
+        </div>
+
+        <CurrentDateTime />
+      </div>
+      <div className=" grid grid-cols-1 sm:grid-cols-4 mt-5  px-4 gap-10 justify-between">
+        <TheMeetingsCards />
+      </div>
+    </div>
+  )
 }
 
 export default Home
