612201a221cdb4fafb09af213bdd00f648655f3c
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-10 03:45:39 +0500

    adding input in the meetings array

--- utils/MeetingCardsArray.ts
+++ utils/MeetingCardsArray.ts
@@ -1,3 +1,7 @@
+import CreateMeeting from '@/components/Inputfields/CreateMeeting'
+import Empty from '@/components/Inputfields/Empty'
+import JoinMeeting from '@/components/Inputfields/JoinMeeting'
+import { Input } from 'postcss'
 import {
   FaVideo,
   FaDoorOpen,
@@ -10,21 +14,25 @@ const meetingCards = [
     icon: FaVideo, // Start Meeting Icon
     heading: 'Start Meeting',
     bgColor: 'bg-blue-800', // Navy Blue
+    Input: CreateMeeting,
   },
   {
     icon: FaDoorOpen, // Join Meeting Icon
     heading: 'Join Meeting',
     bgColor: 'bg-teal-600', // Teal
+    Input: JoinMeeting,
   },
   {
     icon: FaCalendarAlt, // Schedule Meeting Icon
     heading: 'Schedule Meeting',
     bgColor: 'bg-gray-700', // Dark Gray-Blue
+    Input: Empty,
   },
   {
     icon: FaRegFileVideo, // View Recordings Icon
     heading: 'View Recordings',
     bgColor: 'bg-slate-700', // Slate
+    Input: Empty,
   },
 ]
 
