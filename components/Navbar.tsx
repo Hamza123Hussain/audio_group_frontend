82a37bda1d519b490b3f2cdab6c6c6adfcf91c70
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-09 16:21:00 +0500

    Image and Logo added in Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,9 +4,9 @@ import Image from 'next/image'
 const Navbar = () => {
   return (
     <div className=" py-1 px-2 flex justify-between bg-slate-200 text-black shadow-lg shadow-slate-400 ">
-      <div className=" flex items-center gap-2">
-        <Image src={Logo} width={50} height={50} alt="" />
-        <h1>TalkHub</h1>
+      <div className=" flex items-center ">
+        <Image src={Logo} width={30} height={30} alt="" />
+        <h1 className=" text-base">TalkHub</h1>
       </div>
       <h2>aaa</h2>
     </div>
