bb2812bc815c04103f70aa417163ad7030c05b3b
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-10 03:45:54 +0500

    adding the input in the dialog

--- components/SideComponents/Home/TheMeetingsCards.tsx
+++ components/SideComponents/Home/TheMeetingsCards.tsx
@@ -17,18 +17,20 @@ const TheMeetingsCards = () => {
           <Dialog>
             <DialogTrigger>
               <div
-                className={`${element.bgColor} flex flex-col p-2  w-[18vw]   rounded-xl gap-10`}
+                className={`${element.bgColor} hover:border-2 flex flex-col p-2 w-[32vw]  md:w-[18vw]   rounded-xl gap-10`}
               >
                 <div className=" border-2 border-gray-300 w-fit rounded-lg">
                   {<element.icon size={40} />}
                 </div>
-                <h1 className=" text-left font-bold">{element.heading}</h1>
+                <h1 className=" text-left font-bold text-[8px] sm:text-xs md:text-base">
+                  {element.heading}
+                </h1>
               </div>
             </DialogTrigger>
             <DialogContent className=" bg-slate-800">
               <DialogHeader>
                 <DialogTitle></DialogTitle>
-                <DialogDescription></DialogDescription>
+                <DialogDescription>{<element.Input />}</DialogDescription>
               </DialogHeader>
             </DialogContent>
           </Dialog>
