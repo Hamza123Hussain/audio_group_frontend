d795590b44a067ea55509257647571fb6f92ebc1
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-09 20:00:53 +0500

    adding a border to the right of the sidenav bar

--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -1,14 +1,18 @@
 import sideNavItems from '@/utils/SideNavArray'
+import { SIDENAV } from '@/utils/SideNavInterface'
 import React from 'react'
 
-const SideNav = () => {
+const SideNav = ({ setindex, index }: { setindex: any; index: any }) => {
   return (
-    <div className=" flex bg-gray-500 min-h-screen ">
-      <div className="   bg-slate-800  flex flex-col justify-around">
-        {sideNavItems.map((element: any) => (
+    <div className=" flex bg-gray-500 min-h-screen border-r border-gray-50 ">
+      <div className="   bg-slate-800  flex flex-col justify-around ">
+        {sideNavItems.map((element: SIDENAV) => (
           <div
-            key={element.name}
-            className=" px-2   text-white items-center flex gap-2"
+            onClick={() => setindex(element.index)}
+            key={element.index}
+            className={` px-2   text-white items-center flex gap-2 cursor-pointer ${
+              index == element.index ? 'text-blue-500' : ''
+            } `}
           >
             {' '}
             <element.icon />
