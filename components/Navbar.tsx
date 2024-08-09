199d631554c2b87c3768fdbcb1b566fdfe18faba
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-09 16:32:24 +0500

    Navbar completed with style and nesscary componenets

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,30 @@
+'use client'
 import React from 'react'
 import Logo from '../public/Logo.png'
 import Image from 'next/image'
+import { useAuth0 } from '@auth0/auth0-react'
+import UserProfile from './UserPage'
 const Navbar = () => {
+  const { isAuthenticated } = useAuth0()
+  const { loginWithRedirect } = useAuth0()
   return (
-    <div className=" py-1 px-2 flex justify-between bg-slate-200 text-black shadow-lg shadow-slate-400 ">
+    <div className=" py-1 px-2 flex justify-between bg-slate-200 text-black hover:shadow-lg hover:shadow-slate-400 ">
       <div className=" flex items-center ">
         <Image src={Logo} width={30} height={30} alt="" />
-        <h1 className=" text-base">TalkHub</h1>
+        <h1 className=" text-slate-900 font-semibold hover:font-extrabold text-base">
+          TalkHub
+        </h1>
       </div>
-      <h2>aaa</h2>
+      {isAuthenticated ? (
+        <UserProfile />
+      ) : (
+        <button
+          className=" bg-green-400 rounded-lg px-2 text-sm text-white border-white hover:shadow-sm hover:shadow-green-500"
+          onClick={() => loginWithRedirect()}
+        >
+          Log In
+        </button>
+      )}
     </div>
   )
 }
